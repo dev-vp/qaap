2686a024c70765660b5281ae87522106430a67cc
Vincent <vpang@law.fordham.edu>
2021-01-22 16:30:54 -0500

    FE: Basic D3 Rendering Structure

--- client/components/ChartBar.js
+++ client/components/ChartBar.js
@@ -1,8 +1,136 @@
 import React from 'react'
+import * as d3 from 'd3'
 
 class ChartBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.renderBarChart = this.renderBarChart.bind(this)
+  }
+
+  componentDidMount() {
+    this.renderBarChart()
+  }
+
+  componentDidUpdate() {
+    this.renderBarChart()
+  }
+
+  renderBarChart() {
+    const node = this.node
+
+    let width = 600
+    let height = 500
+    let margin = 200
+
+    let svg = d3
+      .select(node)
+      // .append('svg')
+      .attr('width', width + margin) //margin is accounted for because...
+      .attr('height', height + margin) //...the height variable is also used elsewhere
+    // .on('click', () => {console.log(d3.event)})
+
+    let data = [
+      {year: 2011, value: 45},
+      {year: 2012, value: 47},
+      {year: 2013, value: 52},
+      {year: 2014, value: 70},
+      {year: 2015, value: 75},
+      {year: 2016, value: 78}
+    ]
+
+    /* ORIGINALLY - X and Y Scale were predefined here (outside the d3.csv()) */
+    //
+    // // `d3.scaleBand()` computes the scale by dividing the range into uniform bands.
+    // // `.padding()` is the space between each tick mark and bar
+    // let xScale = d3.scaleBand().range([0, width]).padding(0.4) //year (x) axis & padding is the space between ticks
+    // let yScale = d3.scaleLinear().range([height, 0]) //stock price values (y) axis
+
+    let g = svg.append('g').attr('transform', `translate(100, 0)`)
+
+    let xScale = d3
+      .scaleBand() //computes the scale by dividing the range into uniform bands.
+      .domain(
+        data.map(d => {
+          return d.year
+        })
+      ) // array of years being passed in
+      .range([0, width])
+      .padding(0.4) //padding between ticks and the bars
+    let yScale = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(data, d => {
+          return d.value
+        })
+      ])
+      .range([height, 0]) //stock price values (y) axis
+
+    g
+      .append('g')
+      .attr('transform', `translate(0, ${height})`) // x,y axis position (top-left of svg)
+      .call(d3.axisBottom(xScale))
+
+    g
+      .append('g')
+      .call(
+        d3
+          .axisLeft(yScale)
+          .tickFormat(d => {
+            return `$${d}`
+          }) // Prefix our tick label with the dollar-sign ($)
+          .ticks(10) // # of ticks to show
+      )
+      .append('text')
+      .attr('y', 6)
+      .attr('dy', '0.71em')
+      .attr('text-anchor', 'end')
+      .attr('fill', 'black')
+      .attr('font-size', 18) // .attr() or .style() works for font-size
+      .text('value')
+
+    g
+      .selectAll('bar')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar') //add class="bar" attribute
+      .attr('fill', 'steelblue') //fill color
+      // Below, we are passing each data value (year & value) into each of the scale we created earlier (outside the domain). Based on the 'data'(value) we pass in to 'scale()', it is auto-scaled accordingly to the 'range' we set. If the value passed in that is outside the predefined 'range', 'clamping' configuration will be required...
+      .attr('x', d => {
+        return xScale(d.year)
+      }) // position of the bar on the x-axis
+      .attr('y', d => {
+        return yScale(d.value)
+      }) // starting position of the bar on the y-axis
+      .attr('width', xScale.bandwidth()) // returns the width of each band
+      .attr('height', d => {
+        return height - yScale(d.value)
+      })
+    // ^^ height of the SVG minus the corresponding y-value of the bar from the y-scale. Remember that the y-value here would be the tip of the bar since it is calculated from the origin and origin is at (0,0).
+
+    g
+      .append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale))
+      .append('text')
+      .attr('x', width / 2)
+      .attr('y', height - 460)
+      .attr('text-anchor', 'end')
+      .attr('stroke', 'black')
+      .text('Year')
+
+    svg
+      .append('text')
+      .attr('transform', 'translate(100,0)')
+      .attr('x', 50)
+      .attr('y', 50)
+      .attr('font-size', '24px')
+      .text('XYZ Stock Prices')
+  }
+
   render() {
-    return <h1>Bar Graph</h1>
+    return <svg ref={node => (this.node = node)} width={800} height={800} />
   }
 }
 
--- client/components/ChartPie.js
+++ client/components/ChartPie.js
@@ -1,9 +1,92 @@
 import React from 'react'
+import * as d3 from 'd3'
 
-class ChartBar extends React.Component {
+class ChartPie extends React.Component {
+  constructor(props) {
+    super(props)
+    this.renderPieChart = this.renderPieChart.bind(this)
+  }
+
+  componentDidMount() {
+    this.renderPieChart()
+  }
+
+  componentDidUpdate() {
+    this.renderPieChart()
+  }
+
+  renderPieChart() {
+    const node = this.node
+
+    let data = [2, 4, 8, 10]
+    let color = d3.scaleOrdinal([
+      '#4daf4a',
+      '#377eb8',
+      '#ff7f00',
+      '#984ea3',
+      '#e41a1c'
+    ])
+
+    const height = 200
+    const width = 300
+    const radius = Math.min(width, height) / 2
+    // The radius is calculated as `Math.min(width, height) / 2` to ensure that our generated pie will fit into the bounds of the SVG. Therefore, we choose whichever of the width and height is the minimum value.
+
+    const svg = d3
+      .select(node)
+      // .append('svg')
+      .attr('height', height)
+      .attr('width', width)
+
+    // GROUP ELEMENT (to house our pie elements together)
+    const g = svg
+      .append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`)
+
+    //Generate the PIE
+    let pie = d3.pie() //This will generate pies values (i.e. startAngle, endAngle)
+
+    //Generate GROUPS (for each data value/element)
+    let arcs = g
+      .selectAll('arc')
+      .data(pie(data))
+      .enter()
+      .append('g')
+      .attr('class', 'arc')
+
+    // Generate the ARC
+    // (used for drawing paths for each pie wedge)
+    let arc = d3
+      .arc()
+      .innerRadius(0)
+      .outerRadius(radius)
+
+    //Draw ARC PATHs
+    arcs
+      .append('path')
+      .attr('fill', (d, i) => {
+        return color(i)
+      })
+      .attr('d', arc)
+
+    //TEXT LABELS
+    arcs
+      .append('text')
+      .data(pie(data))
+      .attr('color', 'black')
+      .attr('font-size', '20px')
+      .attr('text-anchor', 'middle')
+      .attr('transform', (d, i) => {
+        return `translate(${arc.centroid(d)})`
+      })
+      .text((d, i) => {
+        return d.data
+      })
+  }
+
   render() {
-    return <h1>Pie Chart</h1>
+    return <svg ref={node => (this.node = node)} width={500} height={500} />
   }
 }
 
-export default ChartBar
+export default ChartPie
